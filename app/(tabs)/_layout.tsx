afd441fa6db4d1d2b26e643c71f0c5a36c22bbe5
khankhanfauzan <khankhanfauzan@gmail.com>
2026-04-19 18:35:41 +0800

    feat: implement movie details screen with API integration

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,7 +12,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
                 className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
             >
                 <Image source={icon} tintColor="#151312" className="size-5" />
-                <Text className="text-secondary text-base font-semibold">
+                <Text className="text-secondary text-base font-semibold ml-2">
                     {title}
                 </Text>
             </ImageBackground>
@@ -20,7 +20,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
     }
 
     return (
-        <View className="size-full justify-center items-center">
+        <View className="size-full justify-center items-center mt-4 rounded-full">
             <Image source={icon} tintColor="#A8B5DB" className="size-5" />
         </View>
     );
